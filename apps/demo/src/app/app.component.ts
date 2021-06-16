ed6c1cce8e28b71437a8df99a0f5c0cd307ba2c6
Brad <bcarey@getitc.com>
2021-06-15 23:09:28 -0400

    Use book component

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { GoogleBooksApiService } from '@nxworkspace/data-access/google-books-api';
+import { Book } from '@nxworkspace/data-access/models';
 
 @Component({
   selector: 'nxworkspace-root',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo';
+  searchQuery = new FormControl('');
+  books: Book[] = [];
+
+  constructor(
+    private googleBooksApiService: GoogleBooksApiService,
+  ) {
+  }
+
+  search(searchQuery: string) {
+    this.googleBooksApiService.searchBooks(searchQuery)
+      .subscribe(response => this.books = response);
+  }
 }
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,11 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
